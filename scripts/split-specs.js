cf46ff52e072a759baea5336da71159d2b78eaf3
Vishmi Perera <vishmi@vishmis-mbp.home>
2025-06-02 10:38:40 +0200

    Update the jenkins file for paralell test execution

--- scripts/split-specs.js
+++ scripts/split-specs.js
@@ -5,7 +5,7 @@ const glob = require("glob");
 const index = parseInt(process.argv[2]); // chunk index (0-based)
 const totalChunks = parseInt(process.argv[3]); // total number of chunks
 
-const specs = glob.sync("cypress/e2e/**/*.spec.js").sort();
+const specs = glob.sync("cypress/e2e/tests/**/*.spec.js").sort();
 const selected = specs.filter((_, i) => i % totalChunks === index);
 
 fs.writeFileSync(`chunk-specs-${index}.txt`, selected.join("\n"));
